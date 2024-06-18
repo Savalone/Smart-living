763a31b0c925404fff2085502b90bc55a16e0640
Savalone <luisgsergent@gmail.com>
2024-06-18 05:21:59 -0500

    Fase 2 full

--- main.py
+++ main.py
@@ -36,15 +36,10 @@ def send_ir_signal(signal):
 # Funcion para enviar mensajes
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    texto = "Bienvenido al proyecto Smart-living!!\n\nEscribe  '/menu'  para ver las opciones disponibles."
-    bot.reply_to(message, texto)
-
-#---   MENU    ----------------------------------------------------------------------------------------------------------------------
-
-@bot.message_handler(commands=['menu'])
-def send_menu(message):
-    menu = "Bienvenido al menu de Smart-living.\n\nFunciones disponibles:\n\n/TV\n/Projector\n/AC\n/Share"
-    bot.reply_to(message, menu)
+    img_url = "https://thesustainabilist.ae/wp-content/uploads/2018/07/smart4.jpg"
+    texto = "Bienvenido al proyecto Smart-living!!\n\n\nFunciones disponibles:\n\n/TV\n/Projector\n/AC\n/Clear"
+    imagen = bot.send_photo(chat_id=message.chat.id, photo=img_url, caption=texto)
+    bot.reply_to(message, imagen)
 
 #---  CLEAR  ------------------------------------------------------------------------------------------------------------------------
 
@@ -86,29 +81,93 @@ def callback_query(call):
     if call.data == 'Power':
         tv_signal = "ir universal tv Power\r"
         send_ir_signal(tv_signal)
-        bot.send_message(call.message.chat.id, "Ha sido presionado el botón Power")
+    
     elif call.data == 'Mute':
-        bot.send_message(call.message.chat.id, "Ha sido presionado el botón Mute")
         tv_signal = "ir universal tv Mute\r"
         send_ir_signal(tv_signal)
+    
     elif call.data == 'Chan_up':
         tv_signal = "ir universal tv Ch_next\r"
         send_ir_signal(tv_signal)
-        bot.send_message(call.message.chat.id, "Ha sido presionado el botón de subir canal")
+    
     elif call.data == 'Chan_down':
         tv_signal = "ir universal tv Ch_prev\r"
         send_ir_signal(tv_signal)
-        bot.send_message(call.message.chat.id, "Ha sido presionado el botón de bajar canal")
+       
     elif call.data == 'Vol_up':
         tv_signal = "ir universal tv Vol_up\r"
         send_ir_signal(tv_signal)
-        bot.send_message(call.message.chat.id, "Ha sido presionado el botón de subir volumen")
+        
     elif call.data == 'Vol_down':
         tv_signal = "ir universal tv Vol_dn\r"
         send_ir_signal(tv_signal)
-        bot.send_message(call.message.chat.id, "Ha sido presionado el botón de bajar volumen")
+        
 
-#------------------------------------------------------------------------------------------------------------------------
+#----  Air Conditioner   --------------------------------------------------------------------------------------------------------------------
+
+@bot.message_handler(commands=['AC'])
+def tv_controller(message):
+    menu = types.InlineKeyboardMarkup(row_width=2)
+
+    # Agregar botones
+    btn_power = types.InlineKeyboardButton('Power', callback_data='Power')
+    btn_max = types.InlineKeyboardButton('Max', callback_data='Max')
+    btn_min = types.InlineKeyboardButton('Min', callback_data='Min')
+
+    # Agrega los botones al menu
+    menu.add(btn_max, btn_min, btn_power)
+    bot.send_message(message.chat.id, "Control remoto del Aire acondicionado", reply_markup=menu)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'Power':
+        ac_signal = "ir universal ac Off\r"
+        send_ir_signal(ac_signal)
+    
+    elif call.data == 'Max':
+        ac_signal = "ir universal ac Cool_hi\r"
+        send_ir_signal(ac_signal)
+    
+    elif call.data == 'Min':
+        ac_signal = "ir universal ac Cool_lo\r"
+        send_ir_signal(ac_signal)
+    
+
+#---  Projectors  -----------------------------------------------------------------------------------------------------------------------------
+
+@bot.message_handler(commands=['Projector'])
+def tv_controller(message):
+    menu = types.InlineKeyboardMarkup(row_width=2)
+
+    # Agregar botones
+    btn_power = types.InlineKeyboardButton('Power', callback_data='Power')
+    btn_Mute = types.InlineKeyboardButton('Mute', callback_data='Mute')
+    btn_Vol_down = types.InlineKeyboardButton('-', callback_data='Vol_down')
+    btn_Vol_up = types.InlineKeyboardButton('+', callback_data='Vol_up')
+    
+    # Agrega los botones al menu
+    menu.add(btn_power, btn_Mute, btn_Vol_down, btn_Vol_up)
+    bot.send_message(message.chat.id, "Control remoto del Proyector", reply_markup=menu)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'Power':
+        pr_signal = "ir universal projector Power\r"
+        send_ir_signal(pr_signal)
+    
+    elif call.data == 'Mute':
+        pr_signal = "ir universal projector Mute\r"
+        send_ir_signal(pr_signal)
+    
+    elif call.data == 'Vol_down':
+        pr_signal = "ir universal projector Vol_down\r"
+        send_ir_signal(pr_signal)
+
+    elif call.data == 'Vol_up':
+        pr_signal = "ir universal projector Vol_up\r"
+        send_ir_signal(pr_signal)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
